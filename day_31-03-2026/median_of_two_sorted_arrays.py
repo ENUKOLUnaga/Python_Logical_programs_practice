f4eb3886f3492f284bd59a214172c90fd7ac4bdf
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-03-31 23:16:46 +0530

    added day_31-03-2026:[nagendra] adding logic problems

---
+++ day_31-03-2026/median_of_two_sorted_arrays.py
@@ -0,0 +1,8 @@
+def findMedianSortedArrays(nums1, nums2):
+        arr=sorted(nums1+nums2)
+        n=len(arr)
+
+        if n%2==1:
+            return arr[n//2]
+        else:
+            return (arr[n//2-1]+arr[n//2])/2.0
