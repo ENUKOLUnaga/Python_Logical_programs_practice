2376e926285c36fa92b183ccd382deed0ae3a75e
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-05-26 15:04:53 +0530

    Added mini_projects:[nagendra] count_down task

---
+++ mini_projects/countdown.py
@@ -0,0 +1,13 @@
+import time
+
+def countDown(t):
+    while t:
+        mins,secs=divmod(t,60)
+        timer="{:02d}:{:02d}".format(mins,secs)
+        print(timer,end='\r')
+        time.sleep(1)
+        t-=1
+    print("Eire in the hole!!")
+
+t=input("Enter the time in seconds: ")
+countDown(int(t))
